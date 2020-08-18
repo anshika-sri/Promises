006a12dc009466c160be6786b14abe3308d800f4
anshika-sri <35023358+anshika-sri@users.noreply.github.com>
2020-08-18 15:17:05 +0530

    promise is all rejected as soon as one rejected promise is invoked

---
+++ PromiseIsRejectedAsSoonAsOnePromiseIsRejected.js
@@ -0,0 +1,57 @@
+var Q = require('q');
+
+/**
+ * all promises Logic stats for the defering promise Map,
+ * where is boolean value present, means promise is resolve/rejected
+ */
+var allLogic = {
+  def1: {
+    res: true
+  },
+  def2: {
+    //res is not present/false, means def2 will be rejected
+  }
+};
+
+/** this function may res/rej
+ *  and this function contributes in overall promise if rej/res
+ */ 
+function def1() {
+  var def = Q.defer();
+  setTimeout(function iResolvedAfterTimeout() {
+    console.log('Entering for def1');
+    allLogic.def1.res ? def.resolve('def1 resolved!!!') :
+                         def.reject('def1 rejected!!!')
+  }, 3000 );
+  return def.promise;
+}
+
+/** this function may res/rej
+ *  and this function contributes in overall promise if rej/res
+ */ 
+function def2() {
+  var def = Q.defer();
+  setTimeout(function iRejectedAfterTimeout() {
+    console.log('Entering for def2');
+    allLogic.def2.res ? def.resolve('def2 resolved!!!') :
+                         def.reject('def2 rejected!!!')
+  }, 2000 );
+  return def.promise;
+}
+
+var iniTime = Date.now();
+
+/**
+ * Lets check here for promise all, 
+ * here overall promise is res if all the promisees are resolved
+ * Or, is rej if any one of the promise is rejected.
+ */
+var res =  Q.all([def1(), def2()]);
+res
+ .then(function resolveProceed(res) {
+  console.log('Race winner resolve!!!!', res);
+ })
+ .catch(function rejectError(err) {
+   console.log('Race winner reject!!!!', err);
+   console.log( '$promise.all$, total time is: ', Date.now() - iniTime + ' ms')
+})
